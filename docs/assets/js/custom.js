7cdd5e0f51f731f0a1307a01f20092ebdaf1ff26
jvgiordano <jgiordano100@gmail.com>
2025-05-20 20:22:32 +0200

    Revert "trying to fix navigation"
    This reverts commit 4eb401c11b472ad3edccd7257db9661d35045d16.

--- docs/assets/js/custom.js
+++ docs/assets/js/custom.js
@@ -1,10 +1,10 @@
-// Toggle sidebar when hamburger is clicked
+// Force hamburger menu to be visible and control the sidebar
 document.addEventListener('DOMContentLoaded', function() {
+  // Make sure sidebar is always shown when hamburger is clicked
   var navToggle = document.querySelector('.nav-toggle');
   
   if (navToggle) {
     navToggle.addEventListener('click', function() {
-      document.querySelector('.sidebar').classList.toggle('is-visible');
       document.querySelector('body').classList.toggle('sidebar-visible');
     });
   }
